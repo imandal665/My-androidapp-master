fd46d424e0cd48a75bf073d21c262e3f3cab06cf
imandal665 <37973310+imandal665@users.noreply.github.com>
2020-04-17 18:30:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myandroid/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner.
+    }
+}
